b10518926d03c0734263c24e30ff3e6ed6a63d6f
Serdiuk Serhii <serduksergey90@gmail.com>
2018-06-26 15:12:55 +0300

    complete tasks

--- Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
+++ Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
@@ -18,17 +18,4 @@ public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactor
     public <T extends ViewModel> T create(Class<T> modelClass) {
         return (T) new AddTaskViewModel(mDB, mTaskId);
     }
-
-
-    // TODO (2) Add two member variables. One for the database and one for the taskId
-
-    // TODO (3) Initialize the member variables in the constructor with the parameters received
-
-    // TODO (4) Uncomment the following method
-    // Note: This can be reused with minor modifications
-    /*@Override
-    public <T extends ViewModel> T create(Class<T> modelClass) {
-        //noinspection unchecked
-        return (T) new AddTaskViewModel(mDb, mTaskId);
-    }*/
 }
